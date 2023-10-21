2df1e3a418cfbab38ad20576194d1932f3de1836
Crowdbotics <team@crowdbotics.com>
2023-10-21 00:17:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "musclemetric-44073",
+  url: "https://musclemetric-44073.botics.co",
+  api: "https://musclemetric-44073.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
